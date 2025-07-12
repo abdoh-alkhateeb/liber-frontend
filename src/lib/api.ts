4ac9e83729fa15d6138f2c4e5ebd0515a5b07001
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2025-07-12 14:17:15 +0300

    Add api.ts

---
+++ src/lib/api.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { Thread, Post, Comment } from "../types";
+
+const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export async function fetchThreads(): Promise<Thread[]> {
+  const url = `${baseUrl}/api/threads`;
+  const response = await axios.get(url);
+  return response.data;
+}
+
+export async function fetchPosts(threadId: string): Promise<Post[]> {
+  const url = `${baseUrl}/api/threads/${threadId}/posts`;
+  const response = await axios.get(url);
+  return response.data;
+}
+
+export async function fetchComments(postId: string): Promise<Comment[]> {
+  const url = `${baseUrl}/api/posts/${postId}/comments`;
+  const response = await axios.get(url);
+  return response.data;
+}
+
+export async function createPost(newPost: Post): Promise<Post> {
+  const url = `${baseUrl}/api/posts`;
+  const response = await axios.post(url, newPost);
+  return response.data;
+}
+
+export async function createComment(newComment: Comment): Promise<Comment> {
+  const url = `${baseUrl}/api/comments`;
+  const response = await axios.post(url, newComment);
+  return response.data;
+}
