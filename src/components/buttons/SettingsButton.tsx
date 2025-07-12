d19ff60109c75c1dcb08555f764d85568b6c0761
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2025-07-12 14:17:49 +0300

    Update settings button href

--- src/components/buttons/SettingsButton.tsx
+++ src/components/buttons/SettingsButton.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import { IoMdSettings } from "react-icons/io";
 
 export default function SettingsButton() {
   return (
-    <button className="cursor-pointer transition hover:scale-105 hover:text-blue-600">
+    <Link href="/auth/profile" className="cursor-pointer transition hover:scale-105 hover:text-blue-600">
       <IoMdSettings size="48" />
-    </button>
+    </Link>
   );
 }
