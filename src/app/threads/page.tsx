4e386e82e68c40176e3130c72b964b67b97cca98
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2025-07-12 12:36:22 +0300

    Add page for viewing threads

---
+++ src/app/threads/page.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { Thread } from "@/types";
+import ThreadCard from "@/components/cards/ThreadCard";
+
+const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export default async function ThreadsPage() {
+  let threads: Thread[];
+
+  try {
+    const url = `${baseUrl}/api/threads`;
+    const response = await axios.get(url);
+    threads = response.data;
+  } catch {
+    threads = [];
+  }
+
+  return threads.map((thread) => <ThreadCard key={thread._id} {...thread} />);
+}
